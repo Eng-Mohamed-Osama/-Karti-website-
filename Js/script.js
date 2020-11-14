250e3b4e12568cd5e7f9f6eef31a8b2cae4bcb62
Mohamed Osama Hassan <58494953+Eng-Mohamed-Osama@users.noreply.github.com>
2020-11-14 05:36:30 +0200

    Quick update for site logic

---
+++ Js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+    'use strick';
+
+    $("#header ul li ").on("click", function(e) {
+
+        e.preventDefault();
+
+        $("html, body").animate({
+
+        scrollTop: $('#' + $(this).data('scroll')).offset().top
+
+        }, 700);
+
+    });
+
+    $("#header ul li ").on("click", function() {
+
+        $(this).addClass("act").siblings().removeClass("act");
+
+    });
+
+    $(".navbar-brand").on("click", function() {
+
+        $("html,body").animate({
+
+        scrollTop: 0
+
+        }, 1200);
+
+    });
+});
